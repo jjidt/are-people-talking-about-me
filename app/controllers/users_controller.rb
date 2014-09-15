bccbd29b81d2b43e8ccc5928566c22ecbbba81c0
J.J. Idt & moof mayeda <student@epicodus.com>
2014-09-15 14:39:37 -0700

    json routing with authorization token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :restrict_access, only: :show
   def index
     @users = User.all
   end
@@ -9,5 +10,22 @@ class UsersController < ApplicationController
     @client.search(@user.name, :result_type => "recent").take(20).each do |tweet|
       @tweets << tweet.text
     end
+    respond_to do |format|
+      format.html
+      format.json { render :json => @tweets}
+    end
+  end
+
+private
+  def restrict_access
+    if format_json?
+      authenticate_or_request_with_http_token do |token, options|
+      ApiKey.exists?(access_token: token)
+      end
+    end
+  end
+
+  def format_json?
+    request.format.json?
   end
 end
