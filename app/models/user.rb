8479048e85def765881b0556d7be58079c510941
J.J. Idt & moof mayeda <student@epicodus.com>
2014-09-15 11:33:53 -0700

    generate a user's API key

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :api_key
+
+  after_create :assign_key
+
+private
+  def assign_key
+    ApiKey.create(user_id: self.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915180609) do
+ActiveRecord::Schema.define(version: 20140915182219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "api_keys", force: true do |t|
-    t.string "access_token"
+    t.string  "access_token"
+    t.integer "user_id"
   end
 
   create_table "users", force: true do |t|
